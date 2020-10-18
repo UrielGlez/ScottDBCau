44d72284ff3f2c1bffed68067f07eec435f8b466
UrielGlez <ugj11@hotmail.com>
2020-10-17 18:19:39 -0600

    employee model added

---
+++ src/model/Cl_employee.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+import java.util.Date;
+
+/**
+ *
+ * @author Uriel
+ */
+public class Cl_employee {
+    private int empno;
+    private String ename;
+    private String job;
+    private String mgr;
+    private Date hiredate;
+    private int salary;
+    private int commission;
+    private int deptno;
+    
+    public Cl_employee() { }
+
+    public Cl_employee(int empno, String ename, String job, String mgr, Date hiredate, int salary, int commision, int deptno) {
+        this.deptno = deptno;
+        this.empno = empno;
+        this.job = job;
+        this.mgr = mgr;
+        this.hiredate = hiredate;
+        this.salary = salary;
+        this.ename = ename;
+    }
+
+    public int getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(int empno) {
+        this.empno = empno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getMgr() {
+        return mgr;
+    }
+
+    public void setMgr(String mgr) {
+        this.mgr = mgr;
+    }
+
+    public Date getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(Date hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getCommission() {
+        return commission;
+    }
+
+    public void setCommission(int commission) {
+        this.commission = commission;
+    }
+
+    public int getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(int deptno) {
+        this.deptno = deptno;
+    }
+    
+}
